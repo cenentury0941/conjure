3bd4935fcbe3e75ab053a1658bf13887fa35681f
Cenentury 0941 <cenentury0941@gmail.com>
2023-10-01 00:45:17 +0530

    Added Home and About

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/appbar/AppBar';
+import HomeSlide from './components/home/Home';
+import AboutSlide from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Main-Container'>
+          <HomeSlide></HomeSlide>
+          <AboutSlide></AboutSlide>
+        </div>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../common/Slide.css";
+import "./Home.css";
+
+function HomeSlide(){
+    return (<div className="Slide Home-Slide-Container">
+
+    </div>)
+}
+
+export default HomeSlide

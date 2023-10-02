1a7565d3873b6e5f1a28b84378cbbe8d80edccb0
Cenentury 0941 <cenentury0941@gmail.com>
2023-10-02 23:41:02 +0530

    Future Plans slide text width limited

--- src/components/home/FutureSlide.js
+++ src/components/home/FutureSlide.js
@@ -5,15 +5,15 @@ function FutureSlide() {
 				<div className="Home-Sub-Title-Container Home-Sub-Title-No-Padding">
 					<div className="Home-Heading Home-Heading-Center">Future Plans</div>
 				</div>
-				<div className="Home-Content">
+				<div className="Home-Content-Center">
 					Being mainly powered by Generative AI technology, Conjure is just
 					getting started.
 				</div>
-				<div className="Home-Content">
+				<div className="Home-Content-Center">
 					We plan on further developing Conjure as and when newer and more
 					optimized models are available for specific code generation.
 				</div>
-				<div className="Home-Content">
+				<div className="Home-Content-Center">
 					We also plan on looking into other use cases for AI powered rapid
 					prototyping tools and better means of integrating into the production
 					cycle of projects.

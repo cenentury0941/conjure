87383fd2729f3eabf9b4bf357bef50435162e0d4
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-10-02 12:43:12 +0530

    Update Home page split into seperate components

---
+++ src/components/home/TeamSlide.js
@@ -0,0 +1,14 @@
+function TeamSlide() {
+	return (
+		<div className="Slide Home-Slide-Container Footpath2-Background">
+			<div className="Home-Vertical-Container">
+				<div className="Home-Sub-Title-Container Home-Sub-Title-No-Padding">
+					<div className="Home-Heading Home-Heading-Center">Meet the Team</div>
+				</div>
+				<div className="Home-Content">The team behind Conjure!</div>
+			</div>
+		</div>
+	);
+}
+
+export default TeamSlide;
